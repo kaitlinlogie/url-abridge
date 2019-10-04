3533073286e8c1363c1fe6943b3b3c64a4b51ed3
Jonty Cawley-Graham <jontycg@gmail.com>
2019-10-04 17:12:25 +1300

    Starting to set up models

--- urlabridge/app/models.py
+++ urlabridge/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Domain(models.Model):
+    domain = models.CharField(max_length=200)
+
+
+class Path(models.Model):
+    domain = models.ForeignKey(Domain, on_delete=models.CASCADE)
+    redirect_from = models.CharField(max_length=5000)
+    redirect_to = models.CharField(max_length=1000)
