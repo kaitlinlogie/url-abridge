ac8f056ee4093a605cedba2d927e7d48c3f89be8
Jonty Cawley-Graham <jontycg@gmail.com>
2019-10-11 15:48:02 +1300

    Set up admin panel

--- urlabridge/app/views.py
+++ urlabridge/app/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the url-abridge index.")
+
+
+def redirect(request):
+    return HttpResponse("Hello there")
