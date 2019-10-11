177efdf2c47766cbd4c913c25bee2d8206acaab5
Jonty Cawley-Graham <jontycg@gmail.com>
2019-10-11 17:16:42 +1300

    Super basic version of functionality for redirecting

--- urlabridge/app/views.py
+++ urlabridge/app/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Domain, Path
-from django.db import connection
+from django.shortcuts import redirect
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the url-abridge index.")
-
-
-def redirect(request, path):
+def page_redirect(request, path):
     host_domain = Domain.objects.get(domain='bepure.co')
     redirect_path = Path.objects.get(redirect_from=path, domain=host_domain)
-    return HttpResponse("Redirect from: {}, redirect to: {}".format(redirect_path.redirect_from, redirect_path.redirect_to))
+    return redirect(redirect_path.redirect_to)
