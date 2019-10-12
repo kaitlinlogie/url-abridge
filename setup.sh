3a35fcef7b9f40d6a188350f8bb958516e61e1ba
Jonty Cawley-Graham <jontycg@gmail.com>
2019-10-12 22:07:00 +1300

    Now making migrations before performing them (just incase)

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ source $VENV_DIR/bin/activate
 pip install --upgrade pip --no-cache-dir
 pip install -r requirements.txt --no-cache-dir
 cd urlabridge
+python manage.py makemigrations
 python manage.py migrate
 echo "Please create a super user account"
 python manage.py createsuperuser
