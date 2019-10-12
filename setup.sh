a07cc9a7f63991c90a083f902311668c9d139e97
Jonty Cawley-Graham <jontycg@gmail.com>
2019-10-12 21:20:08 +1300

    Now performing migrations upon set up and prompting to create superuser

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ python3 -m venv $VENV_DIR
 source $VENV_DIR/bin/activate
 pip install --upgrade pip --no-cache-dir
 pip install -r requirements.txt --no-cache-dir
+cd urlabridge
+python manage.py migrate
+python manage.py createsuperuser
